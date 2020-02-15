7dcb32f161ad868c0285b50e226ed265c1c6fcc9
MouseZero <rm@mousezero.com>
2020-02-14 22:53:11 -0700

    Create a test for odd numbers

--- MathService.Tests/UnitTest1.fs
+++ MathService.Tests/UnitTest1.fs
@@ -12,3 +12,9 @@ type TestClass () =
       let expected = Seq.empty<int>
       let actual = MyMath.squaresOfOdds [2; 4; 6; 8; 10]
       Assert.That(expected, Is.EqualTo(actual))
+
+    [<Test>]
+    member this.TestOnesAndEvents() = 
+      let expected = [1; 1; 1; 1]
+      let actual = MyMath.squaresOfOdds [2; 1; 4; 1; 6; 1; 8; 1; 10]
+      Assert.That(actual, Is.EqualTo(expected))
